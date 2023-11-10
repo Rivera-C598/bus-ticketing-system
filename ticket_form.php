55b1654b7c6d3587e2ee5225b5edc8bfe6673e9e
Charlie Shane Rivera <rcharlieshane57@gmail.com>
2023-11-10 11:14:29 +0800

    added simple error message for the ticket form when bus id is not found in the url

--- ticket_form.php
+++ ticket_form.php
@@ -15,57 +15,63 @@
         <?php
         include 'db_config.php';
 
-        $busId = isset($_GET['busId']) ? $_GET['busId'] : '';
+        if (isset($_GET['busId'])) {
+            $busId = isset($_GET['busId']) ? $_GET['busId'] : '';
 
 
-        $query = "SELECT * FROM buses WHERE bus_id = :busId";
-        $stmt = $pdo->prepare($query);
-        $stmt->bindParam(':busId', $busId);
-        $stmt->execute();
+            $query = "SELECT * FROM buses WHERE bus_id = :busId";
+            $stmt = $pdo->prepare($query);
+            $stmt->bindParam(':busId', $busId);
+            $stmt->execute();
 
-        $busDetails = $stmt->fetch(PDO::FETCH_ASSOC);
+            $busDetails = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        if ($busDetails) {
-
-
-            //bus details
-            $airConditioned = $busDetails['air_conditioned'];
-            echo '<div class="bus-details-container" style="font-size: 20px;">';
-            echo '<div class="bus-photo-column">';
-            echo '<img src="' . $busDetails['busPhoto'] . '" alt="Bus Photo" style="max-width: 200px; max-height: 150px;">';
-            echo '</div>';
-            echo '<div class="bus-info-column">';
-            echo '<strong>Bus Plate Number:</strong> ' . $busDetails['plate_number'] . '<br>';
-            echo '<strong>Driver Name:</strong> ' . $busDetails['bus_driver_name'] . '<br>';
-            echo '<strong>Route:</strong> ' . $busDetails['route'] . '<br>';
-            echo '<strong>Air Conditioned</strong> ' . ($airConditioned ? "Yes" : "No") . '<br>';
-            echo '<strong>Available slots: </strong><span id="availableSlots">Loading...</span>';
-            echo '</div>';
-            echo '</div>';
+            if ($busDetails) {
 
 
-            //form for processing tickets -->
-            echo '<form class="ticket-processing-form" action="process_tickets.php" method="post">';
-            echo '<h3 class="text-center">Book Tickets</h3>';
-            echo '<label for="studentId">Student ID:</label>';
-            echo '<input type="text" id="studentId" name="studentId" required maxlength="7">';
-            echo '<label for="busStop">Your Stop:</label>';
-            echo '<select id="busStop" name="busStop" required>';
-            if (strpos($busDetails['route'], 'North') !== false) {
-                echo '<option value="Danao City">Danao City</option>';
-                echo '<option value="Carmen">Carmen</option>';
-                echo '<option value="Catmon">Catmon</option>';
-                echo '<option value="Sogod">Sogod</option>';
-            } elseif (strpos($busDetails['route'], 'South') !== false) {
-                echo '<option value="Compostela">Compostela</option>';
-                echo '<option value="Liloan">Liloan</option>';
-                echo '<option value="Consolacion">Consolacion</option>';
-                echo '<option value="Mandaue">Mandaue</option>';
-                echo '<option value="Cebu">Cebu</option>';
+                //bus details
+                $airConditioned = $busDetails['air_conditioned'];
+                echo '<div class="bus-details-container" style="font-size: 20px;">';
+                echo '<div class="bus-photo-column">';
+                echo '<img src="' . $busDetails['busPhoto'] . '" alt="Bus Photo" style="max-width: 200px; max-height: 150px;">';
+                echo '</div>';
+                echo '<div class="bus-info-column">';
+                echo '<strong>Bus Plate Number:</strong> ' . $busDetails['plate_number'] . '<br>';
+                echo '<strong>Driver Name:</strong> ' . $busDetails['bus_driver_name'] . '<br>';
+                echo '<strong>Route:</strong> ' . $busDetails['route'] . '<br>';
+                echo '<strong>Air Conditioned</strong> ' . ($airConditioned ? "Yes" : "No") . '<br>';
+                echo '<strong>Available slots: </strong><span id="availableSlots">Loading...</span>';
+                echo '</div>';
+                echo '</div>';
+
+
+                //form for processing tickets -->
+                echo '<form class="ticket-processing-form" action="process_tickets.php" method="post">';
+                echo '<h3 class="text-center">Book Tickets</h3>';
+                echo '<label for="studentId">Student ID:</label>';
+                echo '<input type="text" id="studentId" name="studentId" required maxlength="7">';
+                echo '<label for="busStop">Your Stop:</label>';
+                echo '<select id="busStop" name="busStop" required>';
+                if (strpos($busDetails['route'], 'North') !== false) {
+                    echo '<option value="Danao City">Danao City</option>';
+                    echo '<option value="Carmen">Carmen</option>';
+                    echo '<option value="Catmon">Catmon</option>';
+                    echo '<option value="Sogod">Sogod</option>';
+                } elseif (strpos($busDetails['route'], 'South') !== false) {
+                    echo '<option value="Compostela">Compostela</option>';
+                    echo '<option value="Liloan">Liloan</option>';
+                    echo '<option value="Consolacion">Consolacion</option>';
+                    echo '<option value="Mandaue">Mandaue</option>';
+                    echo '<option value="Cebu">Cebu</option>';
+                }
+                echo '</select>';
+                echo '<input type="hidden" id="busId" name="busId" value="' . $busId . '">';
             }
-            echo '</select>';
-            echo '<input type="hidden" id="busId" name="busId" value="' . $busId . '">';
+        }else {
+            echo 'bus Id not found';
         }
+
+
 
         ?>
         <label for="fare">Fare:</label>
